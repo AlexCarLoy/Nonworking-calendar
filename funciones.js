69b35883d7e6b381c4ce1179457859cf4c3490ea
Alejandro Cardona <dalejndro.cl.1910@gmail.com>
2022-09-29 22:50:43 -0600

    Third try: consulta

--- funciones.js
+++ funciones.js
@@ -1,4 +1,3 @@
-// Elementos del DOM
 const $inputDay = document.querySelector('#inputDay');
 const $inputMonth = document.querySelector('#inputMonth');
 const $inputYear = document.querySelector('#inputYear');
@@ -11,50 +10,37 @@ const $btnCalculate = document.querySelector('#btnCalculate');
 
 const $textError = document.querySelector('#textError');
 
-// Constantes
 const WEEKDAYS = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
 
-// Establecer datos por defecto en el DOM (Inicio de la aplicación)
 const date = new Date();
 $inputDay.value = date.getDate();
 $inputMonth.value = date.getMonth();
 $inputYear.value = date.getFullYear();
 
-// Eventos
 $btnCalculate.addEventListener('click', () => {
     const day = $inputDay.value;
     const month = $inputMonth.value;
     const year = $inputYear.value;
 
-    // Validaciones 
     if (day > 31 || day < 1) return $textError.textContent = 'El día debe estar entre 1 y 31';
     if (month > 11 || month < 0) return $textError.textContent = 'El mes debe estar entre 1 y 12';
     if (day > 29 && month === 2 ) return $textError.textContent = 'El día debe estar entre 1 y 31';
 
-    // Fecha completa 
     const date = new Date(year, month, day);
 
-    // Resultados
     const leapYear = isLeapYear(year);
     const isWorkingDay = itsWorkingDay(date.getDay());
     const weekDay = WEEKDAYS[date.getDay()];
 
-    // Mostrar resultados
     $inputLeapYear.value = leapYear ? 'Año bisiesto' : 'Año normal';
     $inputWorkingDay.value = `${weekDay}, ${isWorkingDay ? 'día laborable' : 'fin de semana'}`;
-
-    // Limpiar errores
-    $textError.textContent = '';
 });
 
-
-// Utilidades
 function isLeapYear(year) {
     return new Date(year, 1, 29).getDate() === 29;
 }
 
 function itsWorkingDay(day) {
-    // 0 = Domingo, 6 = Sábado
     switch (day) {
         case 0: case 6:
             return false;
