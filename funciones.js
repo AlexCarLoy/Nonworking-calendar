c9bf930e90228ad177fb9a29d541f95787d11193
Alejandro Cardona <dalejndro.cl.1910@gmail.com>
2022-09-29 22:29:31 -0600

    Third try: consulta

--- funciones.js
+++ funciones.js
@@ -1,38 +1,64 @@
-const dayInput = document.getElementById('day')
-const monthInput = document.getElementById('month')
-const yearInput = document.getElementById('year')
+// Elementos del DOM
+const $inputDay = document.querySelector('#inputDay');
+const $inputMonth = document.querySelector('#inputMonth');
+const $inputYear = document.querySelector('#inputYear');
 
-const calcButton = document.getElementById('calculate')
+const $inputLeapYear = document.querySelector('#inputLeap');
+const $inputWeekDay = document.querySelector('#inputWeekday');
+const $inputWorkingDay = document.querySelector('#inputWorkingDay');
 
-const resultInput = document.getElementById('result')
+const $btnCalculate = document.querySelector('#btnCalculate');
 
-const days = ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"]
+const $textError = document.querySelector('#textError');
 
-const getWorkingDay = (dayOfTheWeek) => {
-    switch (dayOfTheWeek) {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-            return "día laborable"
-            break;
+// Constantes
+const WEEKDAYS = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
 
+// Establecer datos por defecto en el DOM (Inicio de la aplicación)
+const date = new Date();
+$inputDay.value = date.getDate();
+$inputMonth.value = date.getMonth();
+$inputYear.value = date.getFullYear();
+
+// Eventos
+$btnCalculate.addEventListener('click', () => {
+    const day = $inputDay.value;
+    const month = $inputMonth.value;
+    const year = $inputYear.value;
+
+    // Validaciones 
+    if (day > 31 || day < 1) return $textError.textContent = 'El día debe estar entre 1 y 31';
+    if (month > 11 || month < 0) return $textError.textContent = 'El mes debe estar entre 1 y 12';
+    if (day > 29 && month === 2 ) return $textError.textContent = 'El día debe estar entre 1 y 31';
+
+    // Fecha completa 
+    const date = new Date(year, month, day);
+
+    // Resultados
+    const leapYear = isLeapYear(year);
+    const isWorkingDay = itsWorkingDay(date.getDay());
+    const weekDay = WEEKDAYS[date.getDay()];
+
+    // Mostrar resultados
+    $inputLeapYear.value = leapYear ? 'Año bisiesto' : 'Año normal';
+    $inputWorkingDay.value = `${weekDay}, ${isWorkingDay ? 'día laborable' : 'fin de semana'}`;
+
+    // Limpiar errores
+    $textError.textContent = '';
+});
+
+
+// Utilidades
+function isLeapYear(year) {
+    return new Date(year, 1, 29).getDate() === 29;
+}
+
+function itsWorkingDay(day) {
+    // 0 = Domingo, 6 = Sábado
+    switch (day) {
+        case 0: case 6:
+            return false;
         default:
-            return "fin de semana"
-            break;
+            return true;
     }
 }
-
-const calculate = () => {
-    const dt = new Date(yearInput.value, monthInput.value - 1, dayInput.value)
-    const dayOfTheWeek = dt.getDay()
-    resultInput.value = `${days[dayOfTheWeek][0].toUpperCase()+days[dayOfTheWeek].slice(1)}, ${getWorkingDay(dayOfTheWeek)}`
-}
-
-const validateInputs = event => {
-    event.preventDefault()
-    yearInput.value && monthInput.value && dayInput.value ? calculate() : alert('verifica los inputs')
-}
-
-calcButton.addEventListener("click", validateInputs)
